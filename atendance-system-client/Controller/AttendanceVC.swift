ea31cc69714886c9b98b5b0f0a057494b86f7ba8
Raghav Vashisht <raghav2011rv@gmail.com>
2019-10-07 00:53:11 +0530

    Fixed Time 'Table Table' cell and connected UI with placeholder code

---
+++ atendance-system-client/Controller/AttendanceVC.swift
@@ -0,0 +1,36 @@
+//
+//  AttendanceVC.swift
+//  atendance-system-client
+//
+//  Created by Raghav Vashisht on 05/10/19.
+//  Copyright © 2019 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class AttendanceVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let identifier = "attendanceIdentifier"
+    let placeholderName = "EMAT101L"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as? AttendanceCell else { return UITableViewCell() }
+        cell.subjectName.text = placeholderName
+        return cell
+    }
+    
+
+}
---
+++ atendance-system-client/Controller/TimeTableVC.swift
@@ -0,0 +1,48 @@
+//
+//  TimeTableVC.swift
+//  atendance-system-client
+//
+//  Created by Raghav Vashisht on 05/10/19.
+//  Copyright © 2019 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class TimeTableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cellReuseIdentifier = "TTCell"
+    let placeholderName = "Dr. abc xyz"
+    let placeholderSub = "EMAT101L"
+    let placeholderTime = "8:30 AM"
+    let placeholderClass = "A LH 01"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as? TTCell else { return UITableViewCell() }
+        cell.teacherLabel.text = placeholderName
+        cell.subjectLabel.text = placeholderSub
+        cell.timeLabel.text = placeholderTime
+        cell.classLabel.text = placeholderClass
+        print(cell.frame.height)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+
+}
